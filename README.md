036b80ae0c7ae0d39d96d5fdff26fc50ca5e9f4c
triwahyuu <triwahyu34@gmail.com>
2019-10-02 02:21:01 +0700

    adding explaination

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Example STM32 Project with CMake
-This repository uses NUCLEO-STM32L452RE-P board.  
+This repository uses NUCLEO-STM32L452RE-P board.
 
 ## BUILD
 To build the project don't forget to specify the toolchain in the cmake:
@@ -12,4 +12,13 @@ The `libarm_cortexM4lf_math.a` file is obtained from the STM32Cube repository in
 The `STM32L4x2.svd` file is used for debugging, and is obtained from KEIL repository inside `CMSIS/SVD` directory.
 
 ## NOTES
-This repository doesn't mean to be modular, if you want to use it for another board or project you need to customize the [`CMakeLists.txt`](CMakeLists.txt) file.
+This repository doesn't mean to be modular, if you want to use it for another board or project you need to customize the [`CMakeLists.txt`](CMakeLists.txt) file.  
+
+If you want to customize this repository for another project, there are several things that needs to be changed:
+- MCU definition, in [`CMakeLists.txt:47`](CMakeLists.txt#L47).
+- HAL related directory, in [`CMakeLists.txt:26-29`](CMakeLists.txt#L26-L29).
+- CMSIS related directory, in [`CMakeLists.txt:19-21`](CMakeLists.txt#L19-L21).
+- Startup file, in [`CMakeLists.txt:23`](CMakeLists.txt#L23).
+- Linker script, in [`CMakeLists.txt:16`](CMakeLists.txt#L16).
+- ARM FPU math library linkage, in [`CMakeLists.txt:51`](CMakeLists.txt#L51).
+- MCU specific compiler flags, in [`cmake/arm-gcc-flags.cmake:3-6`](cmake/arm-gcc-flags.cmake:L3-L6).
--- cmake/arm-gcc-flags.cmake
+++ cmake/arm-gcc-flags.cmake
@@ -1,8 +1,15 @@
-set(CMAKE_C_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Wall -ffunction-sections -fdata-sections -fomit-frame-pointer -mabi=aapcs -fno-unroll-loops -ffast-math -ftree-vectorize -fstack-usage --specs=nano.specs")
-set(CMAKE_CXX_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -Wall -ffunction-sections -fdata-sections -fomit-frame-pointer -mabi=aapcs -fno-unroll-loops -ffast-math -ftree-vectorize -fno-threadsafe-statics -fno-use-cxa-atexit -fstack-usage --specs=nano.specs")
-set(CMAKE_ASM_FLAGS "-mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -x assembler-with-cpp --specs=nano.specs")
+## compiler flags
 
-SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mabi=aapcs")
+set(CPU_FLAG -mcpu=cortex-m4)
+set(FPU_FLAG -mfpu=fpv4-sp-d16)
+set(FLOAT_ABI -mfloat-abi=hard)
+set(MCU_FLAGS "-mthumb -mabi=aapcs ${CPU_FLAG} ${FPU_FLAG} ${FLOAT_ABI}")
+
+set(CMAKE_C_FLAGS "${MCU_FLAGS} -fno-builtin -Wall -ffunction-sections -fdata-sections -fomit-frame-pointer -fno-unroll-loops -ffast-math -ftree-vectorize -fstack-usage --specs=nano.specs")
+set(CMAKE_CXX_FLAGS "${MCU_FLAGS} -fno-builtin -Wall -ffunction-sections -fdata-sections -fomit-frame-pointer -fno-unroll-loops -ffast-math -ftree-vectorize -fno-threadsafe-statics -fno-use-cxa-atexit -fstack-usage --specs=nano.specs")
+set(CMAKE_ASM_FLAGS "${MCU_FLAGS} -x assembler-with-cpp --specs=nano.specs")
+
+SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections ${MCU_FLAGS}")
 
 SET(CMAKE_C_FLAGS_DEBUG "-O0 -g")
 SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
